4d212be354a66d0805845acf7014bf5abc7853c8
Joel Razuri <jrazuri98@hotmail.com>
2025-01-06 16:27:04 -0300

    commit inicial

---
+++ Semana 1 - Python Avanzado/ejercicio_1.py
@@ -0,0 +1,9 @@
+"""
+    Escribe una función crear_lista que acepte un número variable de argumentos y los almacene en una lista. 
+    Además, debe aceptar un argumento opcional inicial que, si se pasa, se agregue al principio de la lista.
+"""
+
+def crear_lista(*args, opcional=None):
+    
+    
+    pass
