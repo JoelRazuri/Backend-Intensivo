638b49c411261ec6d3657050482d64ae05b567fd
Joel Razuri <jrazuri98@hotmail.com>
2025-01-06 16:47:37 -0300

    ejercicio 1 terminado

--- Semana 1 - Python Avanzado/ejercicio_1.py
+++ Semana 1 - Python Avanzado/ejercicio_1.py
@@ -3,7 +3,32 @@
     Además, debe aceptar un argumento opcional inicial que, si se pasa, se agregue al principio de la lista.
 """
 
-def crear_lista(*args, opcional=None):
+def crear_lista(*args, inicial=None):
     
+    lista = []
+
+    if inicial is not None:
+        lista.append(inicial)
+
+    for i in args:
+        lista.append(i)
     
-    pass
+    return lista
+
+
+# Mejorando el códgio
+# def crear_lista(*args, inicial=None):
+#     lista = [inicial] if inicial is not None else []
+#     lista.extend(args)
+#     return lista
+
+
+print("Lista 1:")
+print(crear_lista(1,2,3,5,"Joel",inicial="Pedro"))
+
+print("------------")
+
+print("Lista 2:")
+print(crear_lista(16,2,33,5,"Joel"))
+
+
