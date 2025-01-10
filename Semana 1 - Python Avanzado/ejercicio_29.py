396bb156ac1eb291b1f9c51806cdb75d913b54eb
Joel Razuri <jrazuri98@hotmail.com>
2025-01-09 22:14:40 -0300

    ejercicio 29 terminado

--- Semana 1 - Python Avanzado/ejercicio_29.py
+++ Semana 1 - Python Avanzado/ejercicio_29.py
@@ -2,3 +2,11 @@
     Generador de cuadrados:
     Escribe un generador cuadrados(n) que devuelva los cuadrados de los números del 1 al n.
 """
+
+def cuadrados(n):
+    for i in range(1, n+1):
+        yield f"{i} : {i**2}"
+
+print("Cuadrados:")
+for cuadrado in cuadrados(5):
+    print(cuadrado)
