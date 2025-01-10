2ad05cb7405c0f00197010365f1d0f057fda937e
Joel Razuri <jrazuri98@hotmail.com>
2025-01-09 22:17:10 -0300

    ejercicio 30 terminado

--- Semana 1 - Python Avanzado/ejercicio_30.py
+++ Semana 1 - Python Avanzado/ejercicio_30.py
@@ -1,3 +1,18 @@
 """
-    
+    Implementa un generador infinito contador_infinito() que devuelva números incrementales empezando desde 1.  
 """
+
+def contador_infinito():
+    contador = 1
+    
+    while True:
+        yield contador
+        contador+=1
+
+
+gen = contador_infinito()
+
+print(next(gen))
+print(next(gen))
+print(next(gen))
+print(next(gen))
