10b9bfab28904a7123990ef518c9f1b2b4d3ba29
Joel Razuri <jrazuri98@hotmail.com>
2025-01-10 22:27:15 -0300

    ejercicio 33 terminado

--- Semana 1 - Python Avanzado/ejercicio 33/main.py
+++ Semana 1 - Python Avanzado/ejercicio 33/main.py
@@ -1,3 +1,9 @@
+"""
+    Crea un archivo operaciones.py que defina funciones para suma, resta, multiplicación y división.
+    Importa este módulo en otro archivo main.py y úsalo para realizar operaciones básicas.
+"""
+
+
 from operaciones import suma, resta, multiplicacion, division
 
 print(suma(10, 5))
