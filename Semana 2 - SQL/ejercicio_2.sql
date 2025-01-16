ba5e1cc2a531dfd7ca4872cc931492bba58c201c
Joel Razuri <jrazuri98@hotmail.com>
2025-01-15 21:35:20 -0300

    ejercicio 2 SQL terminado

---
+++ Semana 2 - SQL/ejercicio_2.sql
@@ -0,0 +1,6 @@
+-- Selecciona todos los registros de la tabla productos.
+-- Selecciona solo los productos cuyo precio sea mayor a 50.
+
+SELECT * FROM productos;
+
+SELECT * FROM productos WHERE precio > 50;
