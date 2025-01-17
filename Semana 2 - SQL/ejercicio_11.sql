f22d056aa181803d7326b1828e5f58446185c779
Joel Razuri <jrazuri98@hotmail.com>
2025-01-16 23:30:00 -0300

    falta hacer

---
+++ Semana 2 - SQL/ejercicio_11.sql
@@ -0,0 +1,5 @@
+-- Expresión CASE: Usando la tabla Ventas, escribe una consulta que añada una columna llamada categoria_precio:
+-- "Alto" si el precio_unitario es mayor a 500.
+-- "Medio" si está entre 300 y 500.
+-- "Bajo" si es menor a 300.
+
